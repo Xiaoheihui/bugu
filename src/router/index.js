657df50af34f3f1711e00826ddec918d0ff82a49
WillWHH <43749376+WillWHH@users.noreply.github.com>
2020-04-16 23:46:57 +0800

    删除部分无用项

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import login from '@/pages/login'
 import register from '@/pages/register'
 import welcome from '@/pages/welcome'
