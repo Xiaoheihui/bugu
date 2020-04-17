7d5f412c3ea1204bdacae465cd866b9a24a38089
scut-log <526726697@qq.com>
2020-04-17 09:16:19 +0800

    优化菜单flex布局

--- src/pages/welcome.vue
+++ src/pages/welcome.vue
@@ -1,30 +1,34 @@
 <template>
   <div class="welcome">
-	  <el-row :gutter="1" class="page">
-		<el-col :span="3" class="aside" :model="userInfo">
-			<img :src="userInfo.avatarUrl" class="myAvatar" alt="用户头像"/>
-			<div class="menu">
-				<el-row v-for="menu in menuList" :key="menu.name" class="menuSub">
-					<el-button :class="{menuOnClick:menu.style,menuUnClick:!menu.style}" :icon="menu.selected" 
-					@click="changeMenu(menu.name)"></el-button>
-				</el-row>
-			</div>
-		</el-col>
-		<el-col :span="6" class="list">
-			<div class="searchNav">
-				<el-input class="searchInput" v-model="searchInput" clearable placeholder="搜索" prefix-icon="el-icon-search">
-					<el-button slot="append" icon="el-icon-search"></el-button>
-				</el-input>
-			</div>
-			<div class="addFriend">
-				<el-button class="addFriendAccess">
-					<i class="addIcon iconfont icon-addFriend_B"></i>
-					<span class="addText">添加好友</span>
+	  <div class="page">
+		  <el-row :gutter="1" class="container">
+			<el-col :span="3" class="aside" :model="userInfo">
+				<el-image :src="userInfo.avatarUrl" class="myAvatar" :fit="fit" alt="用户头像"></el-image>
+				<div class="menu">
+					<el-row v-for="menu in menuList" :key="menu.name" class="menuSub">
+						<el-button :class="{menuOnClick:menu.style,menuUnClick:!menu.style}" :icon="menu.selected" 
+						@click="changeMenu(menu.name)"></el-button>
+					</el-row>
+				</div>
+			</el-col>
+			<el-col :span="6" class="list">
+				<div class="searchNav">
+					<el-input class="searchInput" v-model="searchInput" clearable placeholder="搜索" prefix-icon="el-icon-search">
+						<el-button slot="append" icon="el-icon-search"></el-button>
+					</el-input>
+				</div>
+				<div class="addFriend">
+					<el-button class="addFriend">
+					<div class="addFriendAccess">
+						<i class="addIcon iconfont icon-addFriend_B"></i>
+						<span class="addText">新的好友</span>
+					</div>
 					</el-button>
-			</div>
-		</el-col>
-		<el-col :span="15"><div class="bg-purple">sss</div></el-col>
-	  </el-row>
+				</div>
+			</el-col>
+			<el-col :span="15" class="bg-purple"><div>sss</div></el-col>
+		  </el-row>
+	  </div>
   </div>
 </template>
 
@@ -58,7 +62,8 @@
 				selected:"el-icon-setting",
 				style:false
 			}
-		]
+		],
+		activeNames: ['1']
       }
     },
     methods:{
@@ -73,6 +78,9 @@
 			  }
 		  }
 		  this.menuList=that;
+	  },
+	  errorHandler() {
+	      return true;
 	  }
     }
   }
@@ -85,7 +93,26 @@
     height:100%;
     width:100%;
   }
+  .welcome::before{
+    content:'';
+    position: absolute;
+    top:0;
+    left:0;
+    height:100%;
+    width:100%;
+    background:transparent url(../static/img/bg.jpg) center center no-repeat;
+    filter: blur(25px);
+    z-index: -1;
+    background-size:cover;
+  }
   .page{
+	position: absolute;
+	margin:0vh 18vw;
+	height:99vh;
+	width:64vw;
+	border:2px solid;
+  }
+  .container{
 	height:100%;
 	width:100%;
   }
@@ -98,12 +125,14 @@
 	justify-content: center;
 	align-items: center;
 	z-index:3;
+/* 	border-top-left-radius:20px;
+	border-bottom-left-radius:20px; */
   }
   .myAvatar{
 	position:absolute;
-	top:4vh;
-	height:15vh;
-	width:15vh;
+	top:4%;
+	height:6vw;
+	width:6vw;
 	border-radius:100%;
   }
   .menu{
@@ -138,29 +167,23 @@
 	width:25%;
 	display:flex;
 	flex-direction:column;
-	justify-content: center;
+	justify-content: flex-start;
 	align-items: center;
 	z-index:3;
   }
   .searchNav{
-	position: absolute;
-	top:0.5vh;
-	left:12.5%;
-	width:25%;
+	width:100%;
   }
   .searchNav .searchInput{
 	width:100%;
 	border:0;
   }
   .addFriend{
-	position: absolute;
-	top:6vh;
-	left:12.5%;
-	width:25%;
+	width:100%;
   }
   .addFriend .addFriendAccess{
 	width:100%;
-	height:100%;
+	height:6vh;
 	font-size:16px;
 	display:flex;
 	flex-direction:row;
@@ -169,13 +192,25 @@
   .addIcon{
 	font-size:40px;
 	padding:5px;
+	margin-right:20px;
   }
   .addText{
-	font-size:24px;
+	font-size:18px;
+  }
+  .groupList{
+	width:25%;
   }
   .bg-purple {
     background: #d3dce6;
 	height:100%;
+	width:62.5%;
+	display:flex;
+	flex-direction:column;
+	justify-content: center;
+	align-items: center;
+	z-index:3;
+/* 	border-top-right-radius:20px;
+	border-bottom-right-radius:20px; */
   }
 
 </style>
