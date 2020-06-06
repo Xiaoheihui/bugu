e7e67152bf8d99d15a13a8565cf75e312f7d5e46
scut-log <526726697@qq.com>
2020-06-06 12:56:58 +0800

    聊天文本未同步的小bug修改

--- src/components/right-session.vue
+++ src/components/right-session.vue
@@ -92,7 +92,7 @@
         ></el-input>
 
         <div class="chatBottom">
-          <el-button icon="el-icon-s-promotion" class="sendButton" @click="sendInfo">发送(S)</el-button>
+          <el-button icon="el-icon-s-promotion" class="sendButton" @click.native="sendInfo">发送(S)</el-button>
         </div>
       </div>
     </div>
@@ -424,7 +424,7 @@ export default {
         }
       }
       changeSelectedText(textArea,this.faceList[index]);
-      // console.log(this.faceList[index]);
+      this.textarea=textArea.value;
       return;
     },
     sendInfo() {
